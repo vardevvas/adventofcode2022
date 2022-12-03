36f87b7d157783c33927d80d1c0d33b61726b6d6
Devas <vardevvas@gmail.com>
2022-12-03 15:23:06 +0200

    day3

--- DayTwo.java
+++ DayTwo.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -514,39 +513,49 @@ public class DayTwo {
         values.put("B", 2);
         values.put("C", 3);
         Integer theScore = 0;
-        for (int i = 0; i < input.length; i++) {
-            if (values.get(input[i][0]) > values.get(input[i][1])) {
-                theScore += values.get(input[i][1]);
-            } else if (values.get(input[i][0]) == values.get(input[i][1])) {
-                theScore += 3 + values.get(input[i][1]);
+        for (String[] array : input) {
+            if (values.get(array[0]) > values.get(array[1])) {
+                theScore += values.get(array[1]);
+            } else if (values.get(array[0]) == values.get(array[1])) {
+                theScore += 3 + values.get(array[1]);
             } else {
-                theScore += 6 + values.get(input[i][1]);
+                theScore += 6 + values.get(array[1]);
             }
         }
         return theScore;
     };
-
-    public static Integer test() {
-        Map<String, Integer> values = new HashMap<String, Integer>();
-        values.put("X", 1);
-        values.put("Y", 2);
-        values.put("Z", 3);
-        values.put("A", 1);
-        values.put("B", 2);
-        values.put("C", 3);
-        String[] test = { "A", "Z" };
-        Integer theScore = 0;
-        if (values.get(test[0]) > values.get(test[1])) {
-            theScore += values.get(test[1]);
-        } else if (values.get(test[0]) == values.get(test[1])) {
-            theScore += 3 + values.get(test[1]);
-        } else {
-            theScore += 6 + values.get(test[1]);
-        }
-        return theScore;
-    };
+    // for (int i = 0; i < input.length; i++) {
+    // if (values.get(input[i][0]) > values.get(input[i][1])) {
+    // theScore += values.get(input[i][1]);
+    // } else if (values.get(input[i][0]) == values.get(input[i][1])) {
+    // theScore += 3 + values.get(input[i][1]);
+    // } else {
+    // theScore += 6 + values.get(input[i][1]);
+    // }
+    // }
+    // return theScore;
 
     public static void main(String[] args) {
         System.out.println(guess());
     }
 }
+
+// public static Integer test() {
+// Map<String, Integer> values = new HashMap<String, Integer>();
+// values.put("X", 1);
+// values.put("Y", 2);
+// values.put("Z", 3);
+// values.put("A", 1);
+// values.put("B", 2);
+// values.put("C", 3);
+// String[] test = { "A", "Z" };
+// Integer theScore = 0;
+// if (values.get(test[0]) > values.get(test[1])) {
+// theScore += values.get(test[1]);
+// } else if (values.get(test[0]) == values.get(test[1])) {
+// theScore += 3 + values.get(test[1]);
+// } else {
+// theScore += 6 + values.get(test[1]);
+// }
+// return theScore;
+// };
